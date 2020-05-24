9bceb32a015d5b48b78420002fbd08ae36edecae
Ronald van Bostelen <rvanbostelen@outlook.com>
2020-05-24 19:38:56 +0200

    new branch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var l = new List<Car>
             {
-                new Car("Daimler-AG", "Mercedec CLA"),
-                new Car("Daimler-AG", "Mercedec C-Class"),
-                new Car("Daimler-AG", "Mercedec A-Class")
+                new Car("Daimler-AG", "Mercedes", "CLA"),
+                new Car("Daimler-AG", "Mercedes", "C-Class"),
+                new Car("Daimler-AG", "Mercedes", "A-Class")
             };
 
-            foreach (var car in l)
-            {
-                Console.WriteLine(car);
-            }
+            foreach (var car in l) Console.WriteLine(car);
         }
     }
 }
