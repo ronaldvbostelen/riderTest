70c9696a966cb1fb0528c6d0b7b0afb4f8ebf89e
Ronald van Bostelen <rvanbostelen@outlook.com>
2020-05-24 19:27:06 +0200

    init

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    public class Car
+    {
+        public string Manufactorer { get; }
+        public string Model { get; }
+
+        public Car(string manufactorer, string model)
+        {
+            Manufactorer = manufactorer;
+            Model = model;
+        }
+
+        public override string ToString() => $"{Model}, made by: {Manufactorer}";
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new List<Car>
+            {
+                new Car("Daimler-AG", "Mercedec CLA"),
+                new Car("Daimler-AG", "Mercedec C-Class"),
+                new Car("Daimler-AG", "Mercedec A-Class")
+            };
+
+            foreach (var car in l)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
